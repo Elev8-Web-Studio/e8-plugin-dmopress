e9fd53caeeba17894c6d1d0b96697311623290ea
Jason Pomerleau <jason@p42systems.com>
2016-01-11 14:07:47 -0500

    Added some type validation functions.

--- mu-plugins/tourismhub/functions-global.php
+++ mu-plugins/tourismhub/functions-global.php
@@ -1,6 +1,38 @@
 <?php 
 
-// 
+// Type Validation Functions
 function isValidGoogleAnalyticsID($trackingIdString){
     return preg_match('/^ua-\d{4,9}-\d{1,4}$/i', strval($trackingIdString)) ? true : false;
+}
+
+function isValidInteger($testvalue){
+	if(filter_var($testvalue, FILTER_VALIDATE_INT) !== false){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isValidURL($testvalue){
+	if(filter_var($testvalue, FILTER_VALIDATE_URL) !== false){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isValidEmail($testvalue){
+	if(filter_var($testvalue, FILTER_VALIDATE_EMAIL) !== false){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function isValidFloat($testvalue){
+	if(filter_var($testvalue, FILTER_VALIDATE_FLOAT) !== false){
+		return true;
+	} else {
+		return false;
+	}
 }
