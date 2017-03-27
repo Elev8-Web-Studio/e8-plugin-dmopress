4e0bc9f13f08066ebb7581ab69fff5e8089a750a
jason@jasonpomerleau.com <jason@jasonpomerleau.com>
2017-03-27 16:52:08 -0400

    Fix issue with tag and taxonomy archive pages. Add places post type to tag archive searches.

--- src/setup/hooks.php
+++ src/setup/hooks.php
@@ -7,6 +7,7 @@ function dmopress_filter_the_content( $content ) {
         ob_start();
         $dmopress_template_loader->get_template_part('place','detail');
         $output = ob_get_clean();
+        add_filter( 'the_content', 'dmopress_filter_the_content' );
         return $output;
   }
 
@@ -16,6 +17,7 @@ function dmopress_filter_the_content( $content ) {
         ob_start();
         $dmopress_template_loader->get_template_part('place','archive');
         $output = ob_get_clean();
+        add_filter( 'the_content', 'dmopress_filter_the_content' );
         return $output;
   }
 
@@ -25,6 +27,7 @@ function dmopress_filter_the_content( $content ) {
         ob_start();
         $dmopress_template_loader->get_template_part('place','category');
         $output = ob_get_clean();
+        add_filter( 'the_content', 'dmopress_filter_the_content' );
         return $output;
   }
 
@@ -34,6 +37,7 @@ function dmopress_filter_the_content( $content ) {
         ob_start();
         $dmopress_template_loader->get_template_part('place','feature');
         $output = ob_get_clean();
+        add_filter( 'the_content', 'dmopress_filter_the_content' );
         return $output;
   }
 
@@ -43,9 +47,21 @@ function dmopress_filter_the_content( $content ) {
         ob_start();
         $dmopress_template_loader->get_template_part('place','tag');
         $output = ob_get_clean();
+        add_filter( 'the_content', 'dmopress_filter_the_content' );
         return $output;
   }
 
 }
 add_filter( 'the_content', 'dmopress_filter_the_content' ); 
+
+function tourismpress_add_custom_types( $query ) {
+  if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+    $query->set( 'post_type', array(
+     'post', 'places'
+        ));
+      return $query;
+    }
+}
+add_filter( 'pre_get_posts', 'tourismpress_add_custom_types' );
+
 
