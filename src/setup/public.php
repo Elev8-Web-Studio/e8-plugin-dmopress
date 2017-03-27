1bc5121ac272ba6213983ef8dfe0249886a990e3
jason@jasonpomerleau.com <jason@jasonpomerleau.com>
2017-03-27 10:53:52 -0400

    Move style and script enqueues for front end to single function.

--- src/setup/public.php
+++ src/setup/public.php
@@ -1,14 +1,11 @@
 <?php 
 
-function dmopress_enqueue_css() {
-  wp_enqueue_style('dmopress_css', plugins_url() . '/dmopress/css/dmopress.min.css');
-}
-add_action('init','dmopress_enqueue_css');
 
 function dmopress_enqueue_script() {
 	if(dmo_get_google_maps_api_key() != ''){
     wp_enqueue_script('google-maps', 'https://maps.googleapis.com/maps/api/js?key='.dmo_get_google_maps_api_key());
   }
+  wp_enqueue_style('dmopress_css', plugins_url() . '/dmopress/css/dmopress.min.css');
 }
 
 add_action( 'wp_enqueue_scripts', 'dmopress_enqueue_script' );
