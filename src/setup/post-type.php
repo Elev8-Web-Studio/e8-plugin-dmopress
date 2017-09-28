cdc307853b99765e1278cef49bf4acbe61f4e5b1
jason@jasonpomerleau.com <jason@jasonpomerleau.com>
2017-09-28 13:22:58 -0400

    Remove default support for revisions, excerpt and custom-fields support from Places post type. Themes can always add this support on their own.

--- src/setup/post-type.php
+++ src/setup/post-type.php
@@ -42,7 +42,7 @@ function dmopress_register_places_post_type() {
 								'slug' => 'places'
 							  ),
 		'capability_type'     => 'post',
-		'supports'            => array('title', 'editor', 'author', 'thumbnail', 'comments', 'revisions', 'excerpt', 'custom-fields'),
+		'supports'            => array('title', 'editor', 'author', 'thumbnail', 'comments'),
 	);
 
 	register_post_type( 'places', $args );
