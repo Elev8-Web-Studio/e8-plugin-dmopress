7ebb2bb78f3f01435b30cff5438a76ceaf78dc3c
jason@jasonpomerleau.com <jason@jasonpomerleau.com>
2017-09-28 13:21:45 -0400

    Remove default text labels from event_detail and after_event_event meta templates.

--- src/templates/tribe-eventdetail.php
+++ src/templates/tribe-eventdetail.php
@@ -2,7 +2,6 @@
 $linked_posts = tribe_get_linked_posts(get_the_ID()); 
 foreach($linked_posts['places'] as $post){
     setup_postdata( $post );
-    _e('Location','dmopress_textdomain');
-    echo ': <a href="'.get_permalink( $post ).'">'.$post->post_title.'</a>';
+    echo '<a href="'.get_permalink( $post ).'">'.$post->post_title.'</a>';
 }
 wp_reset_postdata();
